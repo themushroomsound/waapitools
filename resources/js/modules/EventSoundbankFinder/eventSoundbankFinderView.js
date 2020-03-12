588334a641b4b5840acffe732216041ee235c70e
Matthieu Bonneau <bonneau.matthieu@gmail.com>
2020-03-12 18:04:53 +0100

    cleanup up event soundbank finder view (now using reset/populate methods)

--- resources/js/modules/EventSoundbankFinder/eventSoundbankFinderView.js
+++ resources/js/modules/EventSoundbankFinder/eventSoundbankFinderView.js
@@ -3,36 +3,42 @@ class EventSoundbankFinderView extends WwiseObjectView
     constructor(htmlElement)
     {
         super(htmlElement);
+
+        this.inclusionsTable = $(this.htmlElement).find(".soundbankInclusions");
+        this.nbInclusions = $(this.htmlElement).find(".nbInclusions");
     }
 
-    refresh()
+    reset()
     {
-        super.refresh();
+        super.reset();
+        this.nbInclusions.text(0);
+        this.inclusionsTable.html("<tr><td>none</td></tr>");
+    }
 
-        var inclusions = this.wwiseObject ? this.wwiseObject.getSoundBankInclusions() : [];
-        var inclusionsTable = $(this.htmlElement).find(".soundbankInclusions");
+    populate()
+    {
+        super.populate();
 
+        var inclusions = this.wwiseObject.getSoundBankInclusions();
         $(this.htmlElement).find(".nbInclusions").text(inclusions.length);
-        if( inclusions.length < 1 ) {
-            inclusionsTable.html("<tr><td>none</td></tr>");
-        }
 
-        else {
-            inclusionsTable.html("");
-            for( let i=0; i < inclusions.length; i++ )
-            {
-                var newRow = $("#template_inclusionRow").contents().clone();
-                newRow.find(".soundbankName").append( inclusions[i].soundbank.getObjLink(true) );
+        if( inclusions.length < 1 )
+            return;
 
-                if( inclusions[i].object.guid == this.wwiseObject.guid )
-                    newRow.find(".inclusionType").text( "direct inclusion" );
-                else {
-                    newRow.find(".inclusionType").text( "included via " );
-                    newRow.find(".inclusionObjectName").append( inclusions[i].object.getObjLink(true) );
-                }
+        this.inclusionsTable.html("");
+        for( let i=0; i < inclusions.length; i++ )
+        {
+            var newRow = $("#template_inclusionRow").contents().clone();
+            newRow.find(".soundbankName").append( inclusions[i].soundbank.getObjLink(true) );
 
-                newRow.appendTo(inclusionsTable);
+            if( inclusions[i].object.guid == this.wwiseObject.guid )
+                newRow.find(".inclusionType").text( "direct inclusion" );
+            else {
+                newRow.find(".inclusionType").text( "included via " );
+                newRow.find(".inclusionObjectName").append( inclusions[i].object.getObjLink(true) );
             }
+
+            newRow.appendTo(this.inclusionsTable);
         }
     }
 }
