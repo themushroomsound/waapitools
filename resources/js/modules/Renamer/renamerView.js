f20a7fc73128c12f13d19c7123411ea0b75f3da8
Matthieu Bonneau <bonneau.matthieu@gmail.com>
2020-03-13 11:42:04 +0100

    renamer: added table headers + now handling inputs change callbacks

--- resources/js/modules/Renamer/renamerView.js
+++ resources/js/modules/Renamer/renamerView.js
@@ -1,3 +1,8 @@
+class RenamerPendingChangeView extends WwiseObjectView
+{
+
+}
+
 class RenamerView extends WwiseObjectView
 {
     constructor(htmlElement)
@@ -5,12 +10,13 @@ class RenamerView extends WwiseObjectView
         super(htmlElement);
 
         // referencing events list
-        this.referencesTable = $(this.htmlElement).find("#references");
+        this.referencesTable = $(this.htmlElement).find("#references tbody");
         // children list
-        this.childrenTable = $(this.htmlElement).find("#children");
+        this.childrenTable = $(this.htmlElement).find("#children tbody");
         // input fields
         this.findReplaceInputs = $(this.htmlElement).find("#findReplaceInputs");
-
+        this.findInput = $(this.htmlElement).find("#findReplaceInputs #find");
+        this.replaceInput = $(this.htmlElement).find("#findReplaceInputs #replace");
         this.refresh();
     }
 
@@ -18,14 +24,19 @@ class RenamerView extends WwiseObjectView
     {
         super.reset();
         this.findReplaceInputs.hide();
-        this.childrenTable.html("<tr><td>none</td></tr>");
-        this.referencesTable.html("<tr><td>none</td></tr>");
+        this.childrenTable.html("<tr><td>-</td><td>-</td><td>-</td></tr>");
+        this.referencesTable.html("<tr><td>-</td><td>-</td><td>-</td></tr>");
     }
 
     populate()
     {
         super.populate();
+
         this.findReplaceInputs.show();
+        let renamer = this;
+        this.findInput.on('input', function() { renamer.onInputChange(); });
+        this.replaceInput.on('input', function() { renamer.onInputChange(); });
+
         this.populateTable(this.childrenTable, this.wwiseObject.childrenObjects);
         this.populateTable(this.referencesTable, this.wwiseObject.referenceObjects);
     }
@@ -41,7 +52,15 @@ class RenamerView extends WwiseObjectView
             var newRow = $("#template_wwiseObjectRow").contents().clone();
             newRow.find(".type").append( data[index].type );
             newRow.find(".objectName").append( data[index].getObjLink(false) );
+            newRow.find(".newObjectName").append( data[index].getObjLink(false) );
             newRow.appendTo(table);
         }
+    }
+
+    onInputChange()
+    {
+        let findStr = this.findInput.val();
+        let replStr = this.replaceInput.val();
+        console.log("replacing [" + findStr + "] with [" + replStr + "]");
     }
 }
